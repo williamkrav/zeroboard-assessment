64cbd369a2b784724b7d5cb91d98c195cda3c69f
William Lim <william@podifi.ai>
2025-10-04 14:01:37 +0700

    feat: use alembic migration

---
+++ backend/alembic/versions/94bfefd14210_create_logs_table.py
@@ -0,0 +1,44 @@
+"""Create logs table
+
+Revision ID: 94bfefd14210
+Revises: 
+Create Date: 2025-10-04 13:34:32.880150
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = '94bfefd14210'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('logs',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('timestamp', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=True),
+        sa.Column('level', sa.String(length=10), nullable=False),
+        sa.Column('message', sa.Text(), nullable=False),
+        sa.Column('source', sa.String(length=50), nullable=True),
+        sa.Column('endpoint', sa.String(length=100), nullable=True),
+        sa.Column('ip_address', sa.String(length=45), nullable=True),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_logs_id'), 'logs', ['id'], unique=False)
+    op.create_index(op.f('ix_logs_level'), 'logs', ['level'], unique=False)
+    op.create_index(op.f('ix_logs_source'), 'logs', ['source'], unique=False)
+    op.create_index(op.f('ix_logs_timestamp'), 'logs', ['timestamp'], unique=False)
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_index(op.f('ix_logs_timestamp'), table_name='logs')
+    op.drop_index(op.f('ix_logs_source'), table_name='logs')
+    op.drop_index(op.f('ix_logs_level'), table_name='logs')
+    op.drop_index(op.f('ix_logs_id'), table_name='logs')
+    op.drop_table('logs')
